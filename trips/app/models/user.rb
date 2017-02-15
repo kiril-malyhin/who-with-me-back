1335e93b88454809a3c3305fe9500dacd0b3ab18
Kirill.Malyhin <kiril.malyhin@yandex.ru>
2017-02-16 00:35:56 +0300

    new:
    - if user delete profile all his trips we be also deleted

--- trips/app/models/user.rb
+++ trips/app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :trips
+  has_many :trips, dependent: :destroy
   validates :name, uniqueness: true
 end
--- trips/config/routes.rb
+++ trips/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :trips
   resources :users
   post '/user_auth' => 'users#user_auth'
+  post '/find_trip' => 'trips#find_trip'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
